70f51062eaa11ee793d10944355633bbee74540b
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-08-13 22:45:09 +0800

    Add caching to CurrentSessionMixin

--- rider/mixins.py
+++ rider/mixins.py
@@ -1,9 +1,13 @@
 
 
+from functools import cache
+
+
 class CurrentSessionMixin:
     '''
     Provides the `get_current_session` method
     '''
 
+    @cache
     def get_current_session(self):
         return self.request.user.rider.current_session
