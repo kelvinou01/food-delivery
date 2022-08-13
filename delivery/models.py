955bff785b839af7a42aac3c867962c9100a4329
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-08-13 21:22:05 +0800

    Add phone number field to User

--- delivery/models.py
+++ delivery/models.py
@@ -1,4 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
 class User(AbstractUser):
+    username = models.EmailField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    phone_number = PhoneNumberField()
     pass 
